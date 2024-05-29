920f0b6c86206cd8bedd08f0b76fac58c8bcbbd3
Abdykali Azat <azat.abdykalli@gmail.com>
2024-05-29 09:54:51 +0500

    first cargo 3.1 client

---
+++ src/config.js
@@ -0,0 +1,11 @@
+// config.js
+const config = {
+    // apiUrl: 'http://localhost:3001',
+    apiUrl: 'https://first-cargo.kz:3001',
+    
+    nameCargo: 'First-Cargo'
+  };
+  
+  
+export default config;
+  
